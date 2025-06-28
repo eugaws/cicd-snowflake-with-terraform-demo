df1386c66c25972c31c047fb4bfd71818f975357
eugaws <awseugen@gmail.com>
2025-06-28 21:47:08 +0000

    changed

--- prod/main.tf
+++ prod/main.tf
@@ -7,20 +7,20 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "<your-bucket-name>"
-    key            = "terraform-prod.tfstate"
-    region         = "<bucket-region>"
+    bucket         = "my-bucket-eugen-20250627"
+    key            = "terraform-staging.tfstate"
+    region         = "Europe (Frankfurt) eu-central-1"
     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
     # dynamodb_table = "terraform-state-lock-table"
     encrypt        = true
-    role_arn       = "arn:aws:iam::<your-aws-account-no>:role/<terraform-s3-backend-access-role>"
+    role_arn       = "arn:aws:iam::659066864856:user/codespace-terraform"
   }
 }
 
 provider "snowflake" {
   username    = "<your_snowflake_username>"
-  account     = "<your_snowflake_account_identifier>"
-  role        = "<your_snowflake_role>"
+  account     = "pj37934.bgfizbj"
+  role        = "ACCOUNTADMIN"
   private_key = var.snowflake_private_key
 }
 
